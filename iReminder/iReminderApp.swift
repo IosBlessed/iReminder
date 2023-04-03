069ef9bc4721bee998ddf2d033625372e6558dc1
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-04-03 18:11:33 +0000

    Initial setup

---
+++ iReminder/iReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iReminderApp.swift
+//  iReminder
+//
+//  Created by Никита Данилович on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
